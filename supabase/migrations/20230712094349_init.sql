dee6f90a955ad8fa566452c3e95aad9bae184fe5
ArikRunyon <arikrunyon713@hotmail.com>
2023-09-13 18:31:10 -0400

    test

--- supabase/migrations/20230712094349_init.sql
+++ supabase/migrations/20230712094349_init.sql
@@ -5,13 +5,11 @@ create table todos (
   is_complete boolean default false,
   inserted_at timestamp with time zone default timezone('utc'::text, now()) not null
 );
-alter table todos enable row level security;
-create policy "Individuals can create todos." on todos for
-    insert with check (auth.uid() = user_id);
+-- alter table todos enable row level security;
+-- create policy "Individuals can create todos." on todos for
+--     insert with check (auth.uid() = user_id);
 -- create policy "Individuals can view their own todos. " on todos for
 --     select using (auth.uid() = user_id);
-create policy "Individuals can view all todos. " on todos for
-    select using ();
 create policy "Individuals can update their own todos." on todos for
     update using (auth.uid() = user_id);
 create policy "Individuals can delete their own todos." on todos for
