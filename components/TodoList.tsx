7d2fe5c3aa5bce5e81c8657fc8e1cf2f69e8fe7d
ArikRunyon <arikrunyon713@hotmail.com>
2023-09-13 19:28:48 -0400

    test3

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -95,7 +95,7 @@ const Ingredient = ({ todo: ingredient, onDelete }: { todo: Ingredients; onDelet
     <li className="w-full block cursor-pointer hover:bg-gray-200 focus:outline-none focus:bg-gray-200 transition duration-150 ease-in-out">
       <div className="flex items-center px-4 py-4 sm:px-6">
         <div className="min-w-0 flex-1 flex items-center">
-          <div className="text-sm leading-5 font-medium truncate">{ingredient.task}</div>
+          <div className="text-sm leading-5 font-medium truncate">{ingredient.ingredient_name}</div>
         </div>
         <div>
           <input
