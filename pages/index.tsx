a2bf5d3dd20f5a986b74a468ffcf2cce7c97b54e
ArikRunyon <arikrunyon713@hotmail.com>
2023-09-19 18:57:16 -0400

    formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,11 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="w-full h-full bg-gray-200">
+      <div className="bg-gray-200">
         {!session ? (
           <div className="min-w-full min-h-screen flex items-center justify-center">
-            <div className="w-full h-full flex justify-center items-center p-4">
-              <div className="w-full h-full sm:h-auto sm:w-2/5 max-w-sm p-5 bg-white shadow flex flex-col text-base">
+            <div className="flex justify-center items-center p-4">
+              <div className="sm:h-auto sm:w-2/5 max-w-sm p-5 bg-white shadow flex flex-col text-base">
                 <span className="font-sans text-4xl text-center pb-2 mb-1 border-b mx-4 align-center">
                   Login
                 </span>
@@ -29,12 +29,12 @@ export default function Home() {
           </div>
         ) : (
           <div
-            className="w-full h-full flex flex-col justify-center items-center p-4"
+            className="flex flex-col justify-center items-center p-4"
             style={{ minWidth: 250, margin: 'auto' }}
           >
             <IngredientList session={session} />
             <button
-              className="btn-black w-full mt-12"
+              className="btn-black mt-12"
               onClick={async () => {
                 const { error } = await supabase.auth.signOut()
                 if (error) console.log('Error logging out:', error.message)
