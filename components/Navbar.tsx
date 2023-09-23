fb03d8bacd50d07c2d19e17f71fae5a3c314ba04
ArikRunyon <arikrunyon713@hotmail.com>
2023-09-23 18:39:19 -0400

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,31 +1,51 @@
 import { Session, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useRouter } from 'next/router'
 
 export default function Navbar({ session }: { session: Session }) {
 
     const user = session.user
     const supabase = useSupabaseClient()
+    const { push } = useRouter()
 
     return (
         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: '#97935C'}}>
             <div>
                 <button
                     className="btn-black"
-                    style={{margin: '0.5rem'}}>View Ingredients</button>
+                    style={{margin: '0.5rem'}}
+                    onClick={()=>{
+                        push('/viewingredients')
+                    }}>View Ingredients</button>
                 <button
                     className="btn-black"
-                    style={{margin: '0.5rem'}}>Add Ingredient</button>
+                    style={{margin: '0.5rem'}}
+                    onClick={()=>{
+                        push('/addingredient')
+                    }}>Add Ingredient</button>
                 <button
                     className="btn-black"
-                    style={{margin: '0.5rem'}}>View Recipes</button>
+                    style={{margin: '0.5rem'}}
+                    onClick={()=>{
+                        push('/viewrecipes')
+                    }}>View Recipes</button>
                 <button
                     className="btn-black"
-                    style={{margin: '0.5rem'}}>Add Recipe</button>
+                    style={{margin: '0.5rem'}}
+                    onClick={()=>{
+                        push('/addrecipes')
+                    }}>Add Recipe</button>
                 <button
                     className="btn-black"
-                    style={{margin: '0.5rem'}}>View Products</button>
+                    style={{margin: '0.5rem'}}
+                    onClick={()=>{
+                        push('/viewproducts')
+                    }}>View Products</button>
                 <button
                     className="btn-black"
-                    style={{margin: '0.5rem'}}>Add Products</button>
+                    style={{margin: '0.5rem'}}
+                    onClick={()=>{
+                        push('/addproducts')
+                    }}>Add Products</button>
             </div>
             <button
                 className="btn-black"
