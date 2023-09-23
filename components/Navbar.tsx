0639f18027c569887c3f1f517c9a0e9f53d2f864
ArikRunyon <arikrunyon713@hotmail.com>
2023-09-23 17:58:23 -0400

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar({ session }: { session: Session }) {
     const supabase = useSupabaseClient()
 
     return (
-        <div style={{display: 'flex', flexDirection: 'row'}}>
+        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
             <div>
                 <button
                     className="btn-black mt-12">View Ingredients</button>
