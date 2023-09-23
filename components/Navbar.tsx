1f610e36a767be48db48e137f5eb033a50ae0e39
ArikRunyon <arikrunyon713@hotmail.com>
2023-09-23 18:02:41 -0400

    navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,20 +9,20 @@ export default function Navbar({ session }: { session: Session }) {
         <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
             <div>
                 <button
-                    className="btn-black mt-12">View Ingredients</button>
+                    className="btn-black">View Ingredients</button>
                 <button
-                    className="btn-black mt-12">Add Ingredient</button>
+                    className="btn-black">Add Ingredient</button>
                 <button
-                    className="btn-black mt-12">View Recipes</button>
+                    className="btn-black">View Recipes</button>
                 <button
-                    className="btn-black mt-12">Add Recipe</button>
+                    className="btn-black">Add Recipe</button>
                 <button
-                    className="btn-black mt-12">View Products</button>
+                    className="btn-black">View Products</button>
                 <button
-                    className="btn-black mt-12">Add Products</button>
+                    className="btn-black">Add Products</button>
             </div>
             <button
-                className="btn-black mt-12"
+                className="btn-black"
                 onClick={async () => {
                     const { error } = await supabase.auth.signOut()
                     if (error) console.log('Error logging out:', error.message)
